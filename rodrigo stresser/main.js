74253f5c7b2a4536ac8841a0fd0549434b26518d
VendedorDeGalos <127774370+VendedorDeGalos@users.noreply.github.com>
2023-11-17 11:13:21 -0300

    Add files via upload

---
+++ rodrigo stresser/main.js
@@ -0,0 +1,20 @@
+
+function tocasom (idaudio) {
+document.querySelector(idaudio).play();
+}
+
+const listadeteclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listadeteclas.length; contador++ ) {
+
+   const tecla = listadeteclas[contador];
+const instrumento = listadeteclas[contador].classList[1];
+
+
+const idaudio2 = `#som_${instrumento}`;
+
+   listadeteclas[contador].onclick = function (){
+      tocasom(idaudio2);
+   }
+
+}
